ea59a2d1f42113e7aa2bdabf74c0ccfde0cc6da3
goldste6 <goldste6@e605e84e-813e-0410-88cd-b83e9fd653cd>
2009-10-13 00:47:11 +0000

    [GLST] basics of facilities added
    git-svn-id: https://subversion.ews.illinois.edu/svn/GameBuilders/GalacticStrategy@651 e605e84e-813e-0410-88cd-b83e9fd653cd

---
+++ Facility.java
@@ -0,0 +1,8 @@
+public abstract class Facility
+{
+	private static int cost_to_build;
+	private static int time_to_build;
+	
+	public static int getCost_to_build(){return cost_to_build;}
+	public static int getTime_to_build(){return time_to_build;}
+}
--- Planet.java
+++ Planet.java
@@ -5,6 +5,7 @@ public class Planet extends Satellite
 	String name;
 	long population;
 	HashSet<Satellite> satellites;
+	HashSet<Facility> facilities;
 	
 	
 	public Planet(String nm, long pop, int sz, double m, byte habitable)
@@ -18,11 +19,13 @@ public class Planet extends Satellite
 	
 	public void popChange()
 	{
-		//habitablity and polotics/war determines
+		
 	}
 	
 	//methods required for load/save
 	public Planet(){}
 	public HashSet<Satellite> getSatellites(){return satellites;}
 	public void setSatellites(HashSet<Satellite> sat){satellites=sat;}
+	public HashSet<Facility> getFacilities(){return facilities;}
+	public void setFacilities(HashSet<Facility> fac){facilities=fac;}
 }
