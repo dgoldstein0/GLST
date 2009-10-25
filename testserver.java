b42b557a45b4de78e21d40eb538a01d3e7b1b09d
wu68 <wu68@e605e84e-813e-0410-88cd-b83e9fd653cd>
2009-10-25 20:21:27 +0000

    [GLST] network test
    git-svn-id: https://subversion.ews.illinois.edu/svn/GameBuilders/GalacticStrategy@782 e605e84e-813e-0410-88cd-b83e9fd653cd

---
+++ testserver.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.net.*;
+
+
+
+
+public class testserver {
+	
+	public static void main(String args[]) throws IOException  
+    {  
+		
+		ServerSocket serverSocket = null;
+		try {
+		    serverSocket = new ServerSocket(7007);
+		} catch (IOException e) {
+		    System.out.println("Could not listen on port: 7");
+		    System.exit(-1);
+		}
+
+		Socket clientSocket = null;
+		try {
+		    clientSocket = serverSocket.accept();
+		} catch (IOException e) {
+		    System.out.println("Accept failed: 7");
+		    System.exit(-1);
+		}
+		
+		PrintWriter out = new PrintWriter(
+                clientSocket.getOutputStream(), true);
+		BufferedReader in = new BufferedReader(
+				new InputStreamReader(
+                      clientSocket.getInputStream()));
+		
+        BufferedReader stdIn = new BufferedReader(
+                new InputStreamReader(System.in));
+                
+        String userInput;
+
+          	    while ((userInput = stdIn.readLine()) != null) {
+             	    out.println(userInput);
+             	    System.out.println("echo: " + in.readLine());
+          	    }
+		
+		out.close();
+		in.close();
+  	    stdIn.close();
+		serverSocket.close();
+
+    }
+
+}
